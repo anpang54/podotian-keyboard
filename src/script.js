427c08a25a354edc4667ca94458ea35cc36ce11c
anpang54 <ap@anpang.fun>
2025-06-17 21:52:41 +0700

    0.1.1: add support for capital letters
    for desktop, it detects caps lock/shift, while for mobile, there's a caps lock button

--- src/script.js
+++ src/script.js
@@ -2,8 +2,8 @@
 
 // put version
 
-const version = "0.1.0";
-const versionDate = "18 Feb 2025";
+const version = "0.1.1";
+const versionDate = "17 Jun 2025";
 
 document.getElementById("version").innerText = version;
 document.getElementById("versiondate").innerText = versionDate;
@@ -12,17 +12,16 @@ document.getElementById("versiondate").innerText = versionDate;
 // button data
 
 const buttonData = {
-    "a-grave": "à",
-    "n-breve": "n̆",
-    "a-acute": "á",
-    "a-caron": "ǎ",
-    "e-acute": "é",
-    "i-acute": "í",
-    "o-acute": "ó",
-    "u-acute": "ú",
+    "a-grave": ["À", "à"],
+    "n-breve": ["N̆", "n̆"],
+    "a-acute": ["Á", "á"],
+    "a-caron": ["Ǎ", "ǎ"],
+    "e-acute": ["É", "é"],
+    "i-acute": ["Í", "í"],
+    "o-acute": ["Ó", "ó"],
+    "u-acute": ["Ú", "ú"]
 }
 const breakAt = 1;
-
 
 
 // put buttons
@@ -30,7 +29,7 @@ const breakAt = 1;
 let content = "";
 let i = 0;
 for(const [id, display] of Object.entries(buttonData)) {
-    content += `<button onclick="put('${id}')">${display}</button>`;
+    content += `<button id="button-${id}" onclick="put('${id}')"></button>`;
     if(i === breakAt) {
         content += "<br>";
     }
@@ -39,8 +38,49 @@ for(const [id, display] of Object.entries(buttonData)) {
 document.getElementById("buttonsdiv").innerHTML = content;
 
 
+// caps
+
+let capital;
+let capsLock = false;
+let shift = false;
+
+document.body.addEventListener("keydown", function(event) {
+    if(event.key === "Shift") {
+        shift = true;
+    }
+    capital = capsLock ^ shift;
+    updateButtons();
+})
+document.body.addEventListener("keyup", function(event) {
+    if(event.key === "Shift") {
+        shift = false;
+    }
+    if(event.key === "CapsLock") {
+        capsLock = !capsLock;
+    }
+    capital = capsLock ^ shift;
+    updateButtons();
+});
+
+function toggleCaps() {
+    capital = !capital;
+    updateButtons();
+}
+
+
+// update buttons
+
+function updateButtons() {
+    document.getElementById("capsdisplay").innerText = capital? "On":"Off";
+    for(const [id, display] of Object.entries(buttonData)) {
+        document.getElementById(`button-${id}`).innerText = display[capital? 0:1];
+    }
+}
+updateButtons();
+
+
 // put character
 
 function put(id) {
-    document.getElementById("textbox").value += buttonData[id];
+    document.getElementById("textbox").value += buttonData[id][capital? 0:1];
 }
